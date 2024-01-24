17b8580c4f971ef815f72661d7b10c3ec86f2357
Lorena Andrade <lorenaandrade@gmail.com>
2024-01-24 13:57:53 -0300

    controlers

--- Escambo.WebAPI/Model/Usuario.cs
+++ Escambo.WebAPI/Model/Usuario.cs
@@ -3,16 +3,16 @@ public sealed class Usuario
 {
     public int UsuarioId { get; set; }
     public string? Nome { get; set; } //nome
-    public required string? Email{get;set;} //email
-    public required string Senha{ get; set; } //senha
-    public required string? CPF {get; set;}
-    public required string? RG {get; set;}
+    public string? Email{get;set;} //email
+    public string? Senha{ get; set; } //senha
+    public string? CPF {get; set;}
+    public string? RG {get; set;}
     public DateTime Nascimento {get;set;} //aniversario
     public string? Endereço {get; set;} //endereço
     public int Status {get;set;} 
     public decimal Credito {get;set;} //credito
 
     public ICollection<PrestacaoServico> PrestacaoServiços {get;} = new List<PrestacaoServico>();
-    public ICollection<Chat> Chats { get; set; }
+    public ICollection<Chat>? Chats { get; set; }
     public ICollection<Anuncio> Anuncios {get;} = new List<Anuncio>();
 }
