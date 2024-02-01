41e1c587477498e6129ea5868942322046167994
Lorena Andrade <lorenaandrade@gmail.com>
2024-02-01 11:50:21 -0300

    .

--- TechMed.Dommain/Entities/Medico.cs
+++ TechMed.Dommain/Entities/Medico.cs
@@ -3,7 +3,7 @@ namespace TechMed.Dommain.Entities;
 
 public class Medico : Pessoa{
     public int MedicoId {get; set;}
-    // public required string CRM {get; set;}
+    public string? CRM {get; set;}
     // public string? Especialidade {get; set;}
     // public decimal? Salario {get; set;}
     public ICollection<Atendimento>? Atendimentos {get;}
