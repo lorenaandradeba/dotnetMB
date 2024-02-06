7a215b85a5a99a5fc964428c6831eb270c1752fc
Lorena Andrade <lorenaandrade@gmail.com>
2024-02-06 16:53:54 -0300

    autorization

--- TechMed.WebAPI/Program.cs
+++ TechMed.WebAPI/Program.cs
@@ -3,6 +3,8 @@ using TechMed.Aplication.Services;
 using TechMed.Aplication.Services.Interfaces;
 using TechMed.Dommain.Entities;
 using TechMed.Infrastructure.Persistence;
+using TechMed.Aplication.Auth;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,8 +26,33 @@ builder.Services.AddDbContext<TechMedDbContext>(options => {
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+//builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "BasicAuth", Version = "v1" });
+    c.AddSecurityDefinition("basic", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "basic",
+        In = ParameterLocation.Header,
+        Description = "Basic Authorization header using the Bearer scheme."
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "basic"
+                                }
+                            },
+                            new string[] {}
+                    }
+                });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -33,6 +60,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseMiddleware<SimpleAuthHandler>();
 }
 
 app.UseHttpsRedirection();
