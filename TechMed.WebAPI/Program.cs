42aaa20e77f76210b0225d083c44465fc93c897c
Daniel Oliveira <danieloliveira.s558@gmail.com>
2024-02-06 09:42:00 -0300

    atualizando a camada webapi

--- TechMed.WebAPI/Program.cs
+++ TechMed.WebAPI/Program.cs
@@ -1,3 +1,5 @@
+using TechMed.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddDbContext<TechMedContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("TechMed");
+    var serveVersion = ServerVersion.AutoDetect(connectionString);
+    options.UseMySql(connectionString, serveVersion);
+});
+
 
 var app = builder.Build();
 
