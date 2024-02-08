db33f0b4ac6cf988eecdc296e2385994dfdab9a8
Lorena Andrade <lorenaandrade@gmail.com>
2024-02-07 22:48:35 -0300

    correcoes

--- ResTIConnect.Application/Services/Interfaces/IUserService.cs
+++ ResTIConnect.Application/Services/Interfaces/IUserService.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ResTIConnect.Application.InputModels;
 using ResTIConnect.Application.ViewModels;
 
 namespace ResTIConnect.Application.Services.Interfaces
@@ -13,6 +14,7 @@ namespace ResTIConnect.Application.Services.Interfaces
       public List<UserViewModel> GetAll();
       public UserViewModel? GetById(int id);
       public int Create(NewUserInputModel user);
+      public void AdicionaSistemaAoUser(int userId, int sistemaId);
       public void Update(int id, NewUserInputModel user);
       public void Delete(int id);
     }
