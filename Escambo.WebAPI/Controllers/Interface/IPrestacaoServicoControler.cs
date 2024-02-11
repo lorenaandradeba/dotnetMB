ededd176a4b69cd9fe5738ce7f60056cb79dfaf2
Daniel Oliveira <danieloliveira.s558@gmail.com>
2024-02-11 15:19:42 -0300

    Adicioanndo rota PrestacaoServico

---
+++ Escambo.WebAPI/Controllers/Interface/IPrestacaoServicoControler.cs
@@ -0,0 +1,7 @@
+using Escambo.Application.InputModels;
+using Escambo.Application.ViewModels;
+
+namespace Escambo.WebAPI.Controllers.Interface;
+
+public interface IPrestacaoServicoControler:IGenericController<PrestacaoServicoViewModel, PrestacaoServicoInputModel>
+{}
---
+++ Escambo.WebAPI/Controllers/PrestacaoServicoControler.cs
@@ -0,0 +1,53 @@
+using Escambo.Application.InputModels;
+using Escambo.Application.Services.Interfaces;
+using Escambo.Application.ViewModels;
+using Escambo.WebAPI.Controllers.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Escambo.WebAPI.Controllers;
+
+[ApiController]
+[Route("Escambo/")]
+
+
+public class PrestacaoServicoControler : ControllerBase, IPrestacaoServicoControler
+{
+    protected readonly IPrestacaoServicoService _prestacaoServicoService;
+
+    public List<PrestacaoServicoViewModel> _prestacaoServicos => _prestacaoServicoService.GetAll().ToList();
+    public PrestacaoServicoControler(IPrestacaoServicoService prestacaoServicoService) => _prestacaoServicoService = prestacaoServicoService;
+    public IActionResult Create(PrestacaoServicoInputModel input)
+    {
+       var id = _prestacaoServicoService.Create(input);
+       if(id == 0) return BadRequest();
+       return Ok(id);
+    }
+
+    public IActionResult Delete(int id)
+    {
+        _prestacaoServicoService.Delete(id);
+        return Ok();
+    }
+
+    [HttpGet("PrestacaoServico/all")]
+    public IActionResult GetAll()
+    {
+       if(_prestacaoServicos == null) return NotFound();
+       return Ok(_prestacaoServicos);
+    }
+
+     [HttpGet("PrestacaoServico/{id}")]
+    public IActionResult GetById(int id)
+    {
+        var _prestacaoServicos = _prestacaoServicoService.GetById(id);
+        if(_prestacaoServicos is not null)  return Ok(_prestacaoServicos);
+        return NotFound();
+    }
+
+    public IActionResult Update(int id, PrestacaoServicoInputModel input)
+    {
+       _prestacaoServicoService.Update(id, input);
+       return Ok();
+    }
+}
